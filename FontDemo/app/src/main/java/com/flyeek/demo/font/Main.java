ef87628b36c2c470d01d9d64df1d6fed02cf4377
liutianyu <liutianyu@dakele.com>
2015-02-13 17:30:41 +0800

    add basic code for font metrics

--- FontDemo/app/src/main/java/com/flyeek/demo/font/Main.java
+++ FontDemo/app/src/main/java/com/flyeek/demo/font/Main.java
@@ -5,20 +5,18 @@ import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.Rect;
-import android.media.Image;
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.view.ViewTreeObserver;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 
 public class Main extends ActionBarActivity {
 
-    private final String SAMPLE_TEXT = "Font Metrics";
+    private final String SAMPLE_TEXT = "Font";
+    private int baseLineY = 100;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,21 +30,23 @@ public class Main extends ActionBarActivity {
 
         TextView sampleTxtView = (TextView) this.findViewById(R.id.main_sample_write);
         writeText(sampleTxtView, SAMPLE_TEXT);
+        TextView showWriteFontTxtView = (TextView) this.findViewById(R.id.main_info_write);
+        showWriteFontInfo(showWriteFontTxtView, sampleTxtView);
 
     }
 
     private Paint drawText(final ImageView targetImgView, String sourceText) {
         int imgWidth = (this.getWindowManager().getDefaultDisplay().getWidth()
             - 2 * this.getResources().getDimensionPixelOffset(R.dimen.activity_horizontal_margin)) / 2;
-        int imgHeight = this.getResources().getDimensionPixelSize(R.dimen.dimen_main_sampletext_height);
+        int imgHeight = this.getResources().getDimensionPixelSize(R.dimen.main_sampletext_viewheight);
         Bitmap textImg = Bitmap.createBitmap(imgWidth, imgHeight, Bitmap.Config.ARGB_8888);
         Canvas canvas = new Canvas(textImg);
 
         // Draw sample text
         Paint textPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
+        textPaint.setTextSize(this.getResources().getDimensionPixelSize(R.dimen.main_sampletext_fontsize));
         Paint.FontMetrics textFontMetrics = textPaint.getFontMetrics();
         float textBaseX = 0;
-        float baseLineY = 100;
         canvas.drawText(sourceText, textBaseX, baseLineY, textPaint);
 
         // Draw base line
@@ -63,7 +63,7 @@ public class Main extends ActionBarActivity {
         // Draw top line
         Paint topLinePaint = new Paint(Paint.ANTI_ALIAS_FLAG);
         ascentLinePaint.setColor(Color.GREEN);
-        float topLineY = baseLineY + textFontMetrics.ascent;
+        float topLineY = baseLineY + textFontMetrics.top;
         canvas.drawLine(0, topLineY, imgWidth, topLineY, topLinePaint);
 
         // Draw descent line
@@ -75,8 +75,11 @@ public class Main extends ActionBarActivity {
         // Draw bottom line
         Paint bottomLinePaint = new Paint(Paint.ANTI_ALIAS_FLAG);
         bottomLinePaint.setColor(Color.MAGENTA);
-        float bottomLineY = baseLineY + textFontMetrics.ascent;
+        float bottomLineY = baseLineY + textFontMetrics.bottom;
         canvas.drawLine(0, bottomLineY, imgWidth, bottomLineY, bottomLinePaint);
+
+        // Set bitmap to ImageView
+        targetImgView.setImageBitmap(textImg);
 
         return textPaint;
     }
@@ -85,29 +88,64 @@ public class Main extends ActionBarActivity {
         Paint.FontMetrics fontMetrics = sourcePaint.getFontMetrics();
         Rect textBound = new Rect();
         sourcePaint.getTextBounds(sourceText, 0, sourceText.length(), textBound);
+        float ascent = Math.abs(fontMetrics.ascent);
+        float top = Math.abs(fontMetrics.top);
+        float descent = fontMetrics.descent;
+        float bottom = fontMetrics.bottom;
+        float leading = fontMetrics.leading;
+        float textWidth = textBound.width();
+        float textHeight = textBound.height();
+        float lineHeight = textHeight + fontMetrics.leading;
+
+        // TODO refine string concatenation
+        String fontInfoString = "";
+        fontInfoString += "baseline pos: " + baseLineY + "\n";
+        fontInfoString += "ascent: " + ascent + "\n";
+        fontInfoString += "top: " + top + "\n";
+        fontInfoString += "descent: " + descent + "\n";
+        fontInfoString += "bottom: " + bottom + "\n";
+        fontInfoString += "leading: " + leading + "\n";
+        fontInfoString += "text width: " + textWidth + "\n";
+        fontInfoString += "text height: " + textHeight + "\n";
+        fontInfoString += "line height: " + lineHeight;
+        targetTxtView.setText(fontInfoString);
+    }
+
+    private void writeText(TextView targetTxtView, String sourceText) {
+        targetTxtView.setText(sourceText);
+    }
+
+    private void showWriteFontInfo(TextView targetTxtView, TextView sourceTxtView) {
+        Paint sourcePaint = sourceTxtView.getPaint();
+        Paint.FontMetrics fontMetrics = sourcePaint.getFontMetrics();
+        Rect textBound = new Rect();
+        sourceTxtView.getLineBounds(0, textBound);
+        String sourceText = sourceTxtView.getText().toString();
+        sourcePaint.getTextBounds(sourceText, 0, sourceText.length(), textBound);
         float baseLine = textBound.bottom - fontMetrics.descent;
         float ascent = Math.abs(fontMetrics.ascent);
         float top = Math.abs(fontMetrics.top);
         float descent = fontMetrics.descent;
         float bottom = fontMetrics.bottom;
+        float leading = (textBound.height() + fontMetrics.leading) * sourceTxtView
+                .getLineSpacingMultiplier() + sourceTxtView.getLineSpacingExtra()
+                - textBound.height();
         float textWidth = textBound.width();
         float textHeight = textBound.height();
+        float lineHeight = sourceTxtView.getLineHeight();
 
         // TODO refine string concatenation
         String fontInfoString = "";
-        fontInfoString += "baseline: " + baseLine + "\n";
+        fontInfoString += "baseline pos: " + baseLine + "\n";
         fontInfoString += "ascent: " + ascent + "\n";
         fontInfoString += "top: " + top + "\n";
         fontInfoString += "descent: " + descent + "\n";
         fontInfoString += "bottom: " + bottom + "\n";
+        fontInfoString += "leading: " + leading + "\n";
         fontInfoString += "text width: " + textWidth + "\n";
         fontInfoString += "text height: " + textHeight + "\n";
+        fontInfoString += "line height: " + lineHeight;
         targetTxtView.setText(fontInfoString);
-
-    }
-
-    private void writeText(TextView targetTxtView, String sourceText) {
-        targetTxtView.setText(sourceText);
     }
 
     @Override
